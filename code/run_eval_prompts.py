f7adfd41c47b20876f21d028f08d0905cac3af1e
Corentin Kervadec HPC UPF <corentin.kervadec@wanadoo.fr>
2023-02-16 15:55:33 +0100

    change the set of relations evaluated during test

--- code/run_eval_prompts.py
+++ code/run_eval_prompts.py
@@ -12,6 +12,48 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+RELATIONS_TEST = [
+    "P1001",
+    "P101",
+    "P103",
+    "P106",
+    "P108",
+    "P127",
+    "P1303",
+    "P131",
+    "P136",
+    "P1376",
+    "P138",
+    "P140",
+    "P1412",
+    "P159",
+    "P17",
+    "P176",
+    "P178",
+    "P19",
+    "P190",
+    "P20",
+    "P264",
+    "P27",
+    "P276",
+    "P279",
+    "P30",
+    "P36",
+    "P361",
+    "P364",
+    "P37",
+    "P39",
+    "P407",
+    "P413",
+    "P449",
+    "P463",
+    "P47",
+    "P495",
+    "P530",
+    "P740",
+    "P937",
+]
+
 def init_template(prompt_file, relation):
     relation = get_relation_meta(prompt_file, relation)
     return relation['template']
@@ -72,7 +114,7 @@ if __name__ == "__main__":
     if args.output_all_log_probs:
         model.k = len(vocab_subset)
 
-    for relation in os.listdir(args.test_data_dir):
+    for relation in RELATIONS_TEST:
         relation = relation.split(".")[0]
         print("RELATION {}".format(relation))
 
