f688d3fcdec6dffd1a0c19a723991df2f8453138
Corentin Kervadec <corentin.kervadec@gmail.com>
2023-03-07 11:33:49 +0100

    add lev dist

--- code/analyze_prompts.py
+++ code/analyze_prompts.py
@@ -21,7 +21,6 @@ from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA, TruncatedSVD
 from plotly.validators.scatter.marker import SymbolValidator
 
-
 """
 Template utils
 """
@@ -363,6 +362,27 @@ def reduce_proj(df, x, z, sl, c, title, algo, use_symbols=True):
 
     return fig
 
+
+def levenshtein(s1, s2):
+
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+    
+    return previous_row[-1]
 
 if __name__ == "__main__":
 
