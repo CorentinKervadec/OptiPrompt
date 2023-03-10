d016927946bfa7740c0f881518005f32888f2a5c
Corentin Kervadec HPC UPF <corentin.kervadec@wanadoo.fr>
2023-03-10 11:35:59 +0100

    fix

--- code/analyze_prompts.py
+++ code/analyze_prompts.py
@@ -9,9 +9,9 @@ import numpy as np
 from utils import get_relation_meta
 from utils import load_vocab, load_data, batchify, analyze, get_relation_meta
 
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-# import matplotlib  
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib  
 
 
 # import plotly.figure_factory as ff
